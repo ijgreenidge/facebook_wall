fd6ff9b7b0ddb89a2d935969ee40398b7f28677a
ijgreenidge <igreenidge2018@bklawtech.com>
2016-04-12 20:22:39 +0000

    done

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -1,6 +1,16 @@
+/* global $ */
+
+
 $(document).ready(function()
 {
-	$('#message').focus();
 
+	$("#post-btn").click(function() {
+		var message = '<br/><div class="row"><div class="col-md-2"><img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-7.jpg" width="100px"></div><div class="col-md-10">'; 
+		message += "<h3>Imani Greenidge</h3>";
+		message += $("#message").val();
+		message += '</div></div>';
+		$("#wall").prepend(message);
+		
+	});
 	// YOUR CODE GOES HERE
 });
